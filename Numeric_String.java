7cae0c4a33a0cab83e06da224e12d5562b921ce7
Harshita Kudchi <97502736+1091631@users.noreply.github.com>
2022-01-24 20:17:00 +0530

    Add files via upload

---
+++ Numeric_String.java
@@ -0,0 +1,17 @@
+package javastring;
+
+public class Numeric_String {
+
+	public static void main(String[] args) {
+		String s="12Hel3oo4";
+				for(int i=65;i<=90;i++) {
+					char ch=(char)i;
+		System.out.println("Ascii="+i+ "char="+ch);
+					
+	
+					
+				}
+
+	}
+
+}
