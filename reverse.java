344635dc12e58e85d80785a91ecb2a084e1a1829
Harshita Kudchi <97502736+1091631@users.noreply.github.com>
2022-01-24 20:19:28 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,17 @@
+package javastring;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		String str= "Hello Harshita";
+		char ch;
+		System.out.println("The String is: ");
+		System.out.println("Hello Harshita");
+		for(int i=str.length()-1;i>=0;i--) {
+			System.out.println(str.charAt(i));
+		}
+				
+				
+	}
+
+}
