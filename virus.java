4fa6e438db8eb3912778aeee6c784bedbdb195a0
Harshita Kudchi <97502736+1091631@users.noreply.github.com>
2022-01-24 20:20:45 +0530

    Add files via upload

---
+++ virus.java
@@ -0,0 +1,25 @@
+package javastring;
+
+import java.util.Scanner;
+
+public class virus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string: ");
+		String n=sc.nextLine();
+		for(int i=0;i<n.length();i++) {
+			if(i==0)
+			{
+			System.out.print(n.charAt(0)+"");
+			
+			}
+			else if(n.charAt(i)==' ') {
+				System.out.print(n.charAt(i+1)+ "");
+			}
+			}
+
+	}
+
+}
