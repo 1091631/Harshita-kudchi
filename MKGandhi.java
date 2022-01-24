b228213a947daa867b77bb2b4a1bab33b84aa01f
Harshita Kudchi <97502736+1091631@users.noreply.github.com>
2022-01-24 20:16:41 +0530

    Add files via upload

---
+++ MKGandhi.java
@@ -0,0 +1,30 @@
+package javastring;
+
+public class MKGandhi {
+	
+	public static void main(String[] args) {
+		String s="Mahatma Karamchand Gandhi";
+		String s1=" ";
+		String s2=" ";
+		for(int i=0;i<s.length();i++) {
+			char ch =s.charAt(i);
+			if(i==0)
+			{
+				System.out.print(s.charAt(i) +" . ");
+			}
+			if(ch ==' ')
+			{
+				System.out.print(s.charAt(i+1) +" . ");
+				
+				
+			
+		}
+		
+		
+		if(ch ==' ')
+			break;
+		
+		}
+	System.out.print(s.substring(19));
+}
+}
